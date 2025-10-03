f17caa25c33557cfb0ef0524ebe56021f46baa81
olatunji Emmanuel <olatunjie355@gmail.com>
2025-10-03 08:37:00 +0100

    Add Divide.py file

---
+++ Python assignments/Python_cornflakes/Divide.py
@@ -0,0 +1,11 @@
+prompt = int(input("Enter a number: "))
+def square_root(number1):
+	if prompt % 5 == 0:
+		return prompt ** 0.5
+number1 = prompt ** 0.5
+print(number1)
+def divide(prompt):
+	if prompt % 5 != 0:
+		return prompt%5
+number2 = prompt % 5
+print(number2)
