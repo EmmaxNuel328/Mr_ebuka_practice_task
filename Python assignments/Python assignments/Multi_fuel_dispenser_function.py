67d2e96e1cfb51e7fe7529f299135c5c401d424f
Emmax <olatunjie355@gmail.com>
2025-10-21 09:30:39 +0100

    Add python

--- Python assignments/Python assignments/Multi_fuel_dispenser_function.py
+++ Python assignments/Python assignments/Multi_fuel_dispenser_function.py
@@ -1,8 +1,114 @@
 def liter_petrol(product,prompt_liter,transaction = []):
 	if prompt_liter >= 1 and prompt_liter <= 50:
 		amount = 650 * prompt_liter
-	else:
-		print("Liters must be between 1 - 50!!!")
-	transaction.append(f"Product: {product}, Liter: {prompt_liter}L, Amount: N{amount} ") 
-	return transaction
+		choice = f"""
+``````````````````````````````````
+WELCOME TO EMMAX FILLING STATION
+ Customer Transaction receipt	
+ Product:  {product}		
+ Liter:   	{prompt_liter}L		 
+ Amount:      N{amount}	       
+ Thank you for your patronage 	
+_________________________________
+"""
+		transaction.append(choice) 
+		return choice
+	return "Liters must be between 1-50!!!"
+
+
+def amount_petrol(product,prompt_amount,transaction = []):
+	if prompt_amount > 650:
+		liter = prompt_amount/650
+		transaction.append(f"Product: {product}, Liter: {liter}L, Amount: N{prompt_amount} ") 
+		choice1 = f"""
+``````````````````````````````````
+ Customer Transaction receipt	
+ Product:  {product}		
+ Liter:   {liter}L		 
+ Amount:      N{prompt_amount}	       
+ Thank you for your patronage 	
+_________________________________
+"""
+		transaction.append(choice1) 
+		return choice1
+	return "Amount must be above liter price!!!"
+
+
+def liter_diesel(product,prompt_liter,transaction = []):
+	if prompt_liter >= 1 and prompt_liter <= 50:
+		amount = 720 * prompt_liter
+		choice = f"""
+``````````````````````````````````
+WELCOME TO EMMAX FILLING STATION
+ Customer Transaction receipt	
+ Product:  {product}		
+ Liter:   	{prompt_liter}L		 
+ Amount:      N{amount}	       
+ Thank you for your patronage 	
+_________________________________
+"""
+		transaction.append(choice) 
+		return choice
+	return "Liters must be between 1-50!!!"
+
+def amount_diesel(product,prompt_amount,transaction = []):
+	if prompt_amount > 72:
+		liter = prompt_amount/720
+		transaction.append(f"Product: {product}, Liter: {liter}L, Amount: N{prompt_amount} ") 
+		choice1 = f"""
+``````````````````````````````````
+ Customer Transaction receipt	
+ Product:  {product}		
+ Liter:   {liter}L		 
+ Amount:      N{prompt_amount}	       
+ Thank you for your patronage 	
+_________________________________
+"""
+		transaction.append(choice1) 
+		return choice1
+	return "Amount must be above liter price!!!"
+
+
+
+
+def liter_kerosene(product,prompt_liter,transaction = []):
+	if prompt_liter >= 1 and prompt_liter <= 50:
+		amount = 550 * prompt_liter
+		choice = f"""
+``````````````````````````````````
+WELCOME TO EMMAX FILLING STATION
+ Customer Transaction receipt	
+ Product:  {product}		
+ Liter:   	{prompt_liter}L		 
+ Amount:      N{amount}	       
+ Thank you for your patronage 	
+_________________________________
+"""
+		transaction.append(choice) 
+		return choice
+	return "Liters must be between 1-50!!!"
+
+def amount_kerosene(product,prompt_amount,transaction = []):
+	if prompt_amount > 550:
+		liter = prompt_amount/550
+		transaction.append(f"Product: {product}, Liter: {liter}L, Amount: N{prompt_amount} ") 
+		choice1 = f"""
+``````````````````````````````````
+ Customer Transaction receipt	
+ Product:  {product}		
+ Liter:   {liter}L		 
+ Amount:      N{prompt_amount}	       
+ Thank you for your patronage 	
+_________________________________
+"""
+		transaction.append(choice1) 
+		return choice1
+	return "Amount must be above liter price!!!"
+
+
+
+
+
+
+
 		
--- Python assignments/Python assignments/Multi_fuel_dispenser_system.py
+++ Python assignments/Python assignments/Multi_fuel_dispenser_system.py
@@ -1,21 +1,64 @@
 from Multi_fuel_dispenser_function import *
+dashboard = """
+WELCOME TO EMMAX FILLING STATION
+
+1. Buy petroleum
+2. Show transaction history
+"""
+print(dashboard)
+prompt_dashboard = input("Enter operation: ")
 available_petroleum = """
+WELCOME TO EMMAX FILLING STATION
 1. petrol = 650/liter
 2. Diesel = 720/liter
 3. Kerosene = 550/liter
 4. Gas = 480/liter
 """
-print(available_petroleum)
-index = 0
-transaction = []
-prompt = 2
-while prompt != "0":
-	prompt = input("Enter type of fuel you want to buy:  ")
-	collect = input("liter or amount: ")
-	match prompt:
-		case "1": 
-			if collect == "liter":
-				prompt_petrol = int(input("How many liters of petrol are you buying (650/liter): "))
-				print(liter_petrol("petrol",prompt_petrol))
+if prompt_dashboard == "1":
+	print(available_petroleum)
+	index = 0
+	transaction = []
+	prompt = 2
+	while prompt != "0":
+		prompt = input("Enter type of fuel you want to buy:  ")
+		collect = input("liter or amount: ")
+		match prompt:
+			case "1": 
+				if collect == "liter":
+					prompt_petrol = int(input("How many liters of petrol are you buying (650/liter): "))
+					customer_transaction_receipt = liter_petrol("petrol",prompt_petrol)
+					print(customer_transaction_receipt  )
+					index += 1
+				if collect == "amount":
+					prompt_petrol1 = int(input("How much petrol are you buying (650/liter): "))
+					print(amount_petrol("petrol",prompt_petrol1))
+				else:
+					print("INVALID INPUT!!!")
+			case "2":
+				if collect == "liter":
+					prompt_diesel = int(input("How many liters of diesel are you buying (720/liter): "))
+					customer_transaction_receipt = liter_diesel("diesel",prompt_diesel)
+					print(customer_transaction_receipt  )
+				if collect == "amount":
+					prompt_diesel1 = int(input("How much diesel are you buying (720/liter): "))
+					print(amount_petrol("diesel",prompt_diesel1))
+				else:
+					print("INVALID INPUT!!!")
+			case "3": 
+				if collect == "liter":
+					prompt_kerosene = int(input("How many liters of kerosene are you buying (550/liter): "))
+					customer_transaction_receipt = liter_kerosene("kerosene",prompt_kerosene)
+					print(customer_transaction_receipt  )
+				if collect == "amount":
+					prompt_kerosene1 = int(input("How much kerosene are you buying (550/liter): "))
+					print(amount_kerosene("kerosene",prompt_kerosene1))
+				else:
+					print("INVALID INPUT!!!")
+
+
+					
+
+				
+			
 				
 			
